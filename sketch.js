e34b060206ab6d47cfcb58b227111cec5cccdb6f
VarshaV60 <80297374+VarshaV60@users.noreply.github.com>
2021-07-05 14:56:25 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,43 @@
+var ball;
+
+function setup(){
+    database=firebase.database();
+    createCanvas(500,500);
+    ball = createSprite(250,250,10,10);
+    ball.shapeColor = "red";
+    var ballPos=database.ref("ball/position");
+    ballPos.on("value",readPos,showError);
+}
+
+function draw(){
+    background("white");
+    if(keyDown(LEFT_ARROW)){
+        changePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        changePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        changePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        changePosition(0,+1);
+    }
+    drawSprites();
+}
+
+function changePosition(x,y){
+    ball.x = ball.x + x;
+    ball.y = ball.y + y;
+}
+function readPos(data){
+postiton=data.val()
+console.log(postiton);
+ball.x=postiton.x
+ball.y=postiton.y
+
+}
+
+function showError(){
+    console.log("error")
+}
